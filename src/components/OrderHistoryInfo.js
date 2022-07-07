54d68432d6cc153c5d39fb03cd3d21d79eaf3d5f
luhaoqi <1171821456@qq.com>
2022-07-08 01:15:41 +0800

    add: 加上按照时间筛选订单

--- src/components/OrderHistoryInfo.js
+++ src/components/OrderHistoryInfo.js
@@ -3,6 +3,9 @@ import '../css/order_history(style).css'
 import {Link} from 'react-router-dom';
 import {getAllOrder} from "../services/userService";
 import localStorage from "localStorage";
+import {DatePicker} from 'antd';
+
+const {RangePicker} = DatePicker;
 
 export class OrderHistoryInfo extends React.Component {
     headers = ["订单号", "订单金额", "下单时间", "收件人", "收件地址", "联系电话"];
@@ -10,7 +13,9 @@ export class OrderHistoryInfo extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            order: null
+            order: null,
+            startTime: null,
+            endTime: null,
         }
         this.getOrderelem = this.getOrderelem.bind(this);
     }
@@ -33,10 +38,16 @@ export class OrderHistoryInfo extends React.Component {
     }
 
     getOrderelem = (Order) => {
+        const {startTime,endTime} = this.state;
+        console.log(startTime , endTime);
         if (!Order) return;
         let res = [];
         for (let i = 0; i < Order.length; i++) {
-
+            if (this.state.startTime && this.state.endTime)
+            {
+                let t = new Date(Order[i].orderList.time);
+                if (!(startTime <= t && t <= endTime)) continue;
+            }
             let flag = false;
             for (let j = 0; j < Order[i].bookName.length; j++)
                 if (Order[i].bookName[j].toString().toLowerCase().indexOf(this.props.book) > -1) {
@@ -69,11 +80,12 @@ export class OrderHistoryInfo extends React.Component {
 
 
     render() {
-        console.log("key: ",this.props.book);
+        console.log("key: ", this.props.book);
         return (
             <div className={"OrderHistory"}>
                 <div className="main_part">
                     <h3>购买记录</h3>
+
                     <div className="container">
                         <div className="row">
                             <div className="col-lg-3 col-md-3 col-sm-12 col-7 menu">
@@ -91,6 +103,15 @@ export class OrderHistoryInfo extends React.Component {
                                 </ul>
                             </div>
                             <div className="col-lg-9 col-md-9 col-sm-5 col-7 data">
+                                <RangePicker showTime
+                                             onChange={(v) => {
+                                                 this.setState(
+                                                     {
+                                                         startTime: v[0].toDate(),
+                                                         endTime: v[1].toDate(),
+                                                     }
+                                                 )
+                                             }}/>
                                 <table>
                                     <tr>{
                                         this.headers.map(function (title, idx) {
