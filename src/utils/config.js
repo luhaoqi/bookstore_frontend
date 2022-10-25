af967b156b70d89136c749abd143b12576da90e7
luhaoqi <1171821456@qq.com>
2022-10-26 01:02:18 +0800

    update: HTTPS

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,5 +1,5 @@
 export default {
-    backendUrl:'http://localhost:8081',  // 此处写跑后端的主机 IP 及端口
+    backendUrl:'https://localhost:8443',  // 此处写跑后端的主机 IP 及端口
     imageUrl:'../assets/',
     websocketUrl:"ws://localhost:8081/websocket/purchase/",
 };
